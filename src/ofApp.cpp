54b9fc78d3bc305b3cf23dfeeb547801b8c678d3
fmandujano <fmandujano@maquivol.com>
2020-12-02 13:59:45 -0600

    Cambios en clase del 3-Dic. Envia posición de raqueta 2 al servidor

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,48 +3,166 @@
 //--------------------------------------------------------------
 void ofApp::setup()
 {
-	//condiciones iniciales
+	//configurar menu principal
+	estadoApp = EEstadoApp::menu;
+	mainPanel.setup();
+	mainPanel.add(btnConnect.setup("Conectar a partida"));
+	mainPanel.add(btnCreate.setup("Crear partida"));
+	mainPanel.setPosition(ofGetWidth() / 2 - mainPanel.getWidth() / 2,
+		ofGetHeight() / 2 - mainPanel.getHeight() / 2);
 
+	//listeners de los botones 
+	btnCreate.addListener(this, &ofApp::createServer);
+	btnConnect.addListener(this, &ofApp::connectServer);
+
+	//condiciones iniciales de la pelota
 	posPlayer1 = new ofVec2f(30, 30);
+	posPlayer2 = new ofVec2f(ofGetWidth() -15 - 30, 30);
 	posPelota = new ofVec2f(ofGetWidth() / 2, ofGetHeight() / 2);
 	velPelota = new ofVec2f(100, -80);
+}
+
+//configuracion del servidor
+void ofApp::createServer()
+{
+	estadoApp = EEstadoApp::server;
+	udpManager.Create();
+	udpManager.Bind(PORT);
+	udpManager.SetNonBlocking(true);
+}
+
+//configuracion del cliente
+void ofApp::connectServer()
+{
+	estadoApp = EEstadoApp::client;
+	udpManager.Create();
+	udpManager.Connect("127.0.0.1", PORT);
+	udpManager.SetNonBlocking(true);
 }
 
 //--------------------------------------------------------------
 void ofApp::update()
 {
-	//movimiento local del player 1
+	if (estadoApp == EEstadoApp::menu)
+	{
+		//nada, el panel no necesita update
+	}
+	else if (estadoApp == EEstadoApp::server)
+	{
+		updateServer();
+	}
+	else if (estadoApp == EEstadoApp::client)
+	{
+		updateClient();
+	}
+}
+
+void ofApp::updateClient()
+{
+	//movimiento local del player 2, el cliente, siempre a la derecha.
+	if (w)
+		posPlayer2->y -= 200 * ofGetLastFrameTime();
+	if (s)
+		posPlayer2->y += 200 * ofGetLastFrameTime();
+
+	//enviar pos al servidor
+	memset(buffer, 0, BUFFER_SIZE);
+	sprintf(buffer, "%f,%f\n", posPlayer2->x, posPlayer2->y);
+	printf("Enviando : %s", buffer);
+	udpManager.Send(buffer, BUFFER_SIZE);
+}
+
+void ofApp::updateServer()
+{
+	//movimiento local del player 1, el servidor, siempre a la izquierda.
 	if (w)
 		posPlayer1->y -= 200 * ofGetLastFrameTime();
 	if (s)
 		posPlayer1->y += 200 * ofGetLastFrameTime();
+
+	//recibir posicion del jugador 2
+	//imprimir lo que llega:
+	memset(buffer, 0, BUFFER_SIZE);
+	if ( udpManager.Receive(buffer, BUFFER_SIZE) > 0)
+	{
+		printf(" Cliente : %s", buffer);
+	}
+	else
+	{
+		printf(" no llego nada \n", buffer);
+	}
+	
+	
+
 
 	//movimiento de la pelota
 	posPelota->x += velPelota->x * ofGetLastFrameTime();
 	posPelota->y += velPelota->y * ofGetLastFrameTime();
 
 	//colisiones
-	if (posPelota->x <= 0 || posPelota->x >= ofGetWidth())
+	if (posPelota->x <= 0)
 	{
 		velPelota->x *= -1;
+		posPelota->x = 1;
 	}
-	if (posPelota->y <= 0 || posPelota->y >= ofGetHeight())
+	if (posPelota->x >= ofGetWidth())
+	{
+		velPelota->x *= -1;
+		posPelota->x = ofGetWidth() - 1;
+	}
+	if (posPelota->y <= 0)
 	{
 		velPelota->y *= -1;
- 	}
+		posPelota->y = 1;
+	}
+	if (posPelota->y >= ofGetHeight())
+	{
+		velPelota->y *= -1;
+		posPelota->y = ofGetHeight() - 1;
+	}
+}
 
+void ofApp::draw()
+{
+	if (estadoApp == EEstadoApp::menu)
+	{
+		mainPanel.draw();
+	}
+	else if (estadoApp == EEstadoApp::server)
+	{
+		drawServer();
+	}
+	else if (estadoApp == EEstadoApp::client)
+	{
+		drawClient();
+	}
 }
 
 //--------------------------------------------------------------
-void ofApp::draw()
+void ofApp::drawServer()
 {
-	ofSetBackgroundColor(0, 0, 0, 255);
-
+	ofSetBackgroundColor(ofColor::cornflowerBlue);
+	ofSetColor(ofColor::white);
+	ofDrawBitmapString("SERVER MODE", 20, 20);
 	ofSetColor(255, 0, 0);
 	ofCircle(posPelota->x, posPelota->y, 15);
 
 	ofSetColor(255, 255, 255);
 	ofRect( posPlayer1->x, posPlayer1->y, 15, 100);
+	ofRect(posPlayer2->x, posPlayer2->y, 15, 100);
+}
+
+void ofApp::drawClient()
+{
+	ofSetBackgroundColor(0, 0, 0, 255);
+	ofSetColor( ofColor::white);
+	ofDrawBitmapString("CLIENT MODE", 20, 20);
+	ofSetColor(255, 0, 0);
+	ofCircle(posPelota->x, posPelota->y, 15);
+
+	ofSetColor(255, 255, 255);
+	ofRect(posPlayer1->x, posPlayer1->y, 15, 100);
+	ofRect(posPlayer2->x, posPlayer2->y, 15, 100);
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,13 +1,23 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
+#include "ofxNetwork.h"
+#define PORT 6666
+#define BUFFER_SIZE 100
 
 class ofApp : public ofBaseApp{
 
 	public:
 		void setup();
+		void createServer(); // equivale a setupServer
+		void connectServer(); // equvale a setupClient
 		void update();
+		void updateServer();
+		void updateClient();
 		void draw();
+		void drawServer();
+		void drawClient();
 
 		void keyPressed(int key);
 		void keyReleased(int key);
@@ -21,12 +31,28 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 
+		//estado del juego
+		enum EEstadoApp
+		{
+			menu, client, server
+		} estadoApp ;
+
+		//variables del menu
+		ofxPanel mainPanel;
+		ofxButton btnCreate;
+		ofxButton btnConnect;
+
 		//variables de los objetos de juego
 		ofVec2f *posPelota;
 		ofVec2f *velPelota;
 		ofVec2f *posPlayer1;
 		ofVec2f *posPlayer2;
 
+		//inputs
 		bool w, s;
+
+		//Manejo de red
+		ofxUDPManager udpManager;
+		char  buffer[BUFFER_SIZE];
 		
 };
